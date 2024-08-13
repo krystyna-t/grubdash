b595e213df7a7ea5703b1f8ae623b817bbf534b0
krystyna <krystyna@indeed.com>
2024-08-13 21:06:02 +0900

    Implemented dishes controller and router

---
+++ src/dishes/dishes.controller.js
@@ -0,0 +1,118 @@
+const path = require("path");
+
+// Use the existing dishes data
+const dishes = require(path.resolve("src/data/dishes-data"));
+
+// Use this function to assign ID's when necessary
+const nextId = require("../utils/nextId");
+
+// TODO: Implement the /dishes handlers needed to make the tests pass
+
+function dishExists(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Dish does not exist: ${dishId}`,
+  });
+}
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Dish must include a ${propertyName}` });
+  };
+}
+
+function priceIsValidNumber(req, res, next) {
+  const { data: { price } = {} } = req.body;
+  if (price <= 0 || !Number.isInteger(price)) {
+    return next({
+      status: 400,
+      message: `Dish must have a price that is an integer greater than 0`,
+    });
+  }
+  next();
+}
+
+function idMatchesDishId(req, res, next) {
+  const { dishId } = req.params;
+  const { data: { id } = {} } = req.body;
+  if (id && id != dishId) {
+    return next({
+      status: 400,
+      message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
+    });
+  }
+  next();
+}
+
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+function create(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function read(req, res) {
+  res.json({ data: res.locals.dish });
+}
+
+function update(req, res) {
+  const dish = res.locals.dish;
+  const { data: { id, name, description, price, image_url } = {} } = req.body;
+  dish.name = name;
+  dish.description = description;
+  dish.price = price;
+  dish.image_url = image_url;
+
+  res.json({ data: dish });
+}
+
+function destroy(req, res) {
+  const { useId } = req.params;
+  const index = uses.findIndex((use) => use.id === Number(useId));
+  uses.splice(index, 1);
+  res.sendStatus(204);
+}
+
+module.exports = {
+  list,
+  read: [dishExists, read],
+  create: [
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    priceIsValidNumber,
+    create,
+  ],
+  update: [
+    dishExists,
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    priceIsValidNumber,
+    idMatchesDishId,
+    update,
+  ],
+  delete: [dishExists, destroy],
+};
---
+++ src/orders/orders.router.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+
+
+// TODO: Implement the /orders routes needed to make the tests pass
+
+module.exports = router;
